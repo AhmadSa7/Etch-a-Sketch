1f6b7b8378780ea26b9a28cf2b17b74cf4d93f6b
Ahmad Saeed <a7mads3eed2004@gmail.com>
2025-08-04 22:14:17 +0300

    Add default grid and Add button to generate new one based on input

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
 let container = document.querySelector(".container");
 
-for (i = 0; i < 256; i++) {
-    let square = document.createElement('div'); 
-    square.classList.add('square');   
-    container.appendChild(square);
+let btn = document.querySelector("button");
 
-    square.addEventListener("mouseenter", () => {
-    square.style.backgroundColor = 'lightblue';
-    });
+function drawGrid(container, gridNumber) {
 
-    
-    // square.addEventListener('mouseleave', () => {
-    // square.style.backgroundColor = ''; 
-    // });
+    for (i = 0; i < gridNumber * gridNumber; i++) {
+        let square = document.createElement('div'); 
+        square.classList.add('square');   
+        container.appendChild(square);
+
+        square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = 'lightblue';
+        });
+    }
 }
 
+drawGrid(container, 16);
+
+btn.addEventListener("click", () => {
+    let newGridNumber = parseInt(prompt("Enter number of squares per side :"));
+    if (newGridNumber != null && newGridNumber <= 100 && newGridNumber > 0) {
+
+        container.replaceChildren();
+        
+        for (i = 0; i < newGridNumber * newGridNumber; i++) {
+
+            let square = document.createElement('div'); 
+            square.classList.add("square");
+            square.style.flex = `0 0 calc(100% / ${newGridNumber})`;
+
+
+            container.appendChild(square);
+            square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = 'lightblue';
+            });
+        }
+        // let squares = document.querySelectorAll('.square');
+        // squares.style.flex = `0 0 calc(100% / 64)`;
+
+
+
+    } else {
+        alert("Please Enter a number between 1-100");
+    }
+    
+});
+            
