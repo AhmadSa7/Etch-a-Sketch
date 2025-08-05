11ae3e33258211781959a7547f3ee35f12096d8d
Ahmad Saeed <a7mads3eed2004@gmail.com>
2025-08-05 18:39:09 +0300

    Display random RGB color when hovering, Add darkening effect in each interaction

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ let container = document.querySelector(".container");
 
 let btn = document.querySelector("button");
 
+function randomRGB() {
+    let randomR = Math.floor(Math.random() * 256);
+    let randomG = Math.floor(Math.random() * 256);
+    let randomB = Math.floor(Math.random() * 256);
+
+    return `rgb(${randomR}, ${randomG}, ${randomB})`;
+}
+
 function drawGrid(container, gridNumber) {
 
     for (i = 0; i < gridNumber * gridNumber; i++) {
@@ -10,7 +18,17 @@ function drawGrid(container, gridNumber) {
         container.appendChild(square);
 
         square.addEventListener("mouseover", () => {
-        square.style.backgroundColor = 'lightblue';
+        
+            // square.style.backgroundColor = 'lightblue';
+
+            let RandomRGB = randomRGB();
+
+            square.style.backgroundColor = RandomRGB;
+
+            let newOpacity = Number(square.style.opacity);
+            newOpacity = newOpacity + 0.1;            
+
+            square.style.opacity = newOpacity.toString();
         });
     }
 }
@@ -29,17 +47,19 @@ btn.addEventListener("click", () => {
             square.classList.add("square");
             square.style.flex = `0 0 calc(100% / ${newGridNumber})`;
 
-
             container.appendChild(square);
             square.addEventListener("mouseenter", () => {
-            square.style.backgroundColor = 'lightblue';
+                        
+                // square.style.backgroundColor = 'lightblue';
+
+                square.style.backgroundColor = randomRGB();
+
+                let newOpacity = Number(square.style.opacity);
+                newOpacity = newOpacity + 0.1;            
+
+                square.style.opacity = newOpacity.toString();
             });
         }
-        // let squares = document.querySelectorAll('.square');
-        // squares.style.flex = `0 0 calc(100% / 64)`;
-
-
-
     } else {
         alert("Please Enter a number between 1-100");
     }
